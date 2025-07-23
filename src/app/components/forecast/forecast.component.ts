f42804cfdbda67a04b6ca2753e93bf6c31ed8353
marc-nacpil <marc.nacpil@icloud.com>
2025-07-23 17:07:17 +0800

    create weather related components

---
+++ src/app/components/forecast/forecast.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast',
+  imports: [],
+  templateUrl: './forecast.component.html',
+  styleUrl: './forecast.component.css'
+})
+export class ForecastComponent {
+
+}
